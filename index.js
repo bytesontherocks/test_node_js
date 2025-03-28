dadc41d1b9d7527eecb2910b70fc107459aba281
BytesOnTheRocks <bytesontherocks@gmail.com>
2025-03-28 10:14:56 +0000

    s1

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
-// index.js
-import http from 'http';
+import express from 'express';
+import { createServer } from 'node:http';
 
-const server = http.createServer((req, res) => {
-  res.end('Hello from Node.js!');
+const app = express();
+const server = createServer(app);
+
+app.get('/', (req, res) => {
+  res.send('<h1>Hello world</h1>');
 });
 
 server.listen(3000, () => {
-  console.log('Server is running at http://localhost:3000');
+  console.log('server running at http://localhost:3000');
 });
--- readme.md
+++ readme.md
@@ -13,10 +13,13 @@ npm -v
 ./install_nodejs_extensions.sh
 '''
 
-
+web framework for Node.js v4
 '''
 npm install express@4
 npm fund
 '''
 
+'''
+node index.js
+'''
 
